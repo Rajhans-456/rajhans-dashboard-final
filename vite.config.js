ab693cfd767b65b18d050aa0c3e9d2bd55a4a575
Abhinav Lakkapragada <abhinavlakkapragada17@gmail.com>
2025-08-05 04:04:47 -0400

    Fix: Use dist output for Vercel compatibility

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   build: {
-    outDir: "public",
+    outDir: "dist",
   },
   plugins: [react(), expressPlugin()],
   resolve: {
